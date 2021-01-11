5553c5d68aa5bb22c03b02e88cdc9e3bfb04fad8
AudriusLunys <lunysaudrius@inbox.lt>
2021-01-11 14:22:44 +0200

    Uzdavinukai pasikartojimui

--- src/main/java/Antras/AntrasMain.java
+++ src/main/java/Antras/AntrasMain.java
@@ -5,5 +5,29 @@ package Antras;
         Create object, set the values to the object field,
         add object to array. Bonus: add the ability to edit or delete object from the array. */
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AntrasMain {
+    public static void main(String[] args) {
+        ArrayList<Student> students = new ArrayList<>();
+        students.add(createStudent());
+
+        for (Student student : students) {
+            System.out.println(student.getName()+" Age: "+student.getAge()+" Grade: "+student.getGrade());
+        }
+
+    }
+    private static Student createStudent (){
+           Scanner scanner = new Scanner(System.in);
+           Student student = new Student();
+           System.out.println("Student Name : ");
+           student.setName(scanner.nextLine());
+           System.out.println("Student Age : ");
+           student.setAge(scanner.nextInt());
+           System.out.println("Student Grade : ");
+           student.setGrade(scanner.nextInt());
+           return student;
+       }
+
 }
